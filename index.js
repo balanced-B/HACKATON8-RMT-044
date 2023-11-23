3b64a2ad7eb078aa8164eab24a6052e70c045b80
balanced-B <ariobimo.mp@gmail.com>
2023-11-23 23:23:38 +0700

    tambahan fungsi cart dan deleted cart

--- index.js
+++ index.js
@@ -56,23 +56,94 @@ let cars = [
     img: "https://cdn.motor1.com/images/mgl/OozxwY/s3/2024-lamborghini-aventador-successor-rendering.jpg",
   },
 ];
-console.log("test");
+// console.log("test");
+let keranjang = []
+
+let AM9HkKnPurTNXc4PP = document.getElementById('cart')
+AM9HkKnPurTNXc4PP.innerHTML = ''
+
 
 function render(array){
     let wadah=document.getElementById('wadah')
     wadah.innerHTML='';
     
     for (let car of cars) {
-        console.log(car)
+        // console.log(car)
       wadah.innerHTML+=`<div id="${car.id}" class="box-product">
       <img src="${car.img}" alt="${car.name}">
       <h3> Stock : ${car.stock}</h3>
       <h3> harga : ${car.harga}</h3>
+      <button onclick="buyCar(${car.id})">Buy now</button>
     </div>` 
     
     }
 }
 render(cars)
+
+function renderCart (array) {
+    let cart = document.getElementById('cart')
+    cart.innerHTML = ''
+
+    for (const item of keranjang) {
+        cart.innerHTML+= ` <div>
+        <div class="groupNameandImage" id="groupNameandImage">
+            <h3 class="namaProduct" id="namaProduct">
+                ${item.name}
+            </h3>
+            <h4>type: -</h4>
+            <img src="${item.img}" alt="${item.name}" class="imageProduct" id="imageProduct">
+            <p>Spesifikasi: -</p>
+            <p>9999</p>
+            <p>Xcc</p>
+            <p>warna mobil</p>
+        </div>
+        <div class="groupTambahKurang" id="groupTambahKurang">
+            <div class="count" id="count">
+                ${item.stock}
+            </div>
+        </div>
+        <div class="jumlah" id="jumlah">
+            ${item.stock * item.harga}
+        </div>
+        <div class="delete" id="delete">
+            <a onclick="deleteProduct(${item.id})" href="#">Delete</a>
+        </div>
+    </div>`
+    }
+    
+}
+
+
+function buyCar(id) {
+    // console.log(id)
+    for (const car of cars) {
+        if (car.stock > 0) {
+
+            if (id === car.id) {
+                let productFound = false
+                for (const cartItem of keranjang) {
+                    if (cartItem.id === id) {
+                        productFound = true
+                        cartItem.stock++
+                        car.stock--
+                    }
+                }
+                if (productFound === false) {
+                    let newCar = {...car}
+                    newCar.stock = 1
+                    car.stock--
+                    keranjang.push(newCar)
+                }
+            }
+        } else {
+            alert('Lariiis~')
+        }
+    }
+    render(cars)
+    renderCart(keranjang)
+    // console.log(cart)
+}
+
 
 function showCart() {
     let cart = document.getElementById("cart");
@@ -84,3 +155,21 @@ function showCart() {
     }
    }
 
+function deleteProduct(id) {
+    // console.log(id)
+    let deletedKeranjang = []
+    for (const item of keranjang) {
+        if (item.id === id) {
+            for (const car of cars) {
+                if (car.id === item.id) {
+                    car.stock+= item.stock
+                }
+            }
+        } else {
+            deletedKeranjang.push(item)
+        }
+    }
+    keranjang = deletedKeranjang
+    render(cars)
+    renderCart(keranjang)
+}
